179c9d18c1490b1f3075e89a3ef3a462e6cbfb0c
GD <60940875+GlebDen@users.noreply.github.com>
2020-03-04 15:33:07 +0300

    Add files via upload

---
+++ test_gesture.py
@@ -0,0 +1,34 @@
+from time import sleep
+import machine
+from machine import Pin, I2C
+from const import *
+from device import uAPDS9960 as APDS9960
+
+sda_pin = machine.Pin(21)
+scl_pin = machine.Pin(22)
+rst_pin = Pin(19, Pin.OUT)
+
+bus = machine.I2C(sda=sda_pin, scl=scl_pin)
+apds = APDS9960(bus)
+
+dirs = {
+    APDS9960_DIR_NONE: "none",
+    APDS9960_DIR_LEFT: "left",
+    APDS9960_DIR_RIGHT: "right",
+    APDS9960_DIR_UP: "up",
+    APDS9960_DIR_DOWN: "down",
+    APDS9960_DIR_NEAR: "near",
+    APDS9960_DIR_FAR: "far",
+}
+
+apds.setProximityIntLowThreshold(50)
+
+print("Gesture Test")
+print("============")
+apds.enableGestureSensor()
+
+while True:
+    sleep(0.5)
+    if apds.isGestureAvailable():
+        motion = apds.readGesture()
+        print("Gesture={}".format(dirs.get(motion, "unknown")))
---
+++ test_prox.py
@@ -0,0 +1,27 @@
+from time import sleep
+import machine
+from machine import Pin, I2C
+from const import *
+from device import uAPDS9960 as APDS9960
+
+sda_pin = machine.Pin(21)
+scl_pin = machine.Pin(22)
+rst_pin = Pin(19, Pin.OUT)
+
+bus = machine.I2C(sda=sda_pin, scl=scl_pin)
+
+apds = APDS9960(bus)
+
+apds.setProximityIntLowThreshold(50)
+
+print("Proximity Sensor Test")
+print("=====================")
+apds.enableProximitySensor()
+
+oval = -1
+while True:
+    sleep(0.25)
+    val = apds.readProximity()
+    if val != oval:
+        print("proximity={}".format(val))
+        oval = val
